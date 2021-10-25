e1e4055c09ad424af7d51b1803d91e7dec299c49
Yassine37199 <yassine.ouri7319@gmail.com>
2021-10-25 06:11:37 +0100

    finished booking form section

--- src/main.jsx
+++ src/main.jsx
@@ -288,6 +288,28 @@ const MainPage = () => (
                                     <input type="email" className="form__input" placeholder="Email Adress" id="email" required/>
                                     <label className="form__label" for="email">Email Adress</label>
                                 </div>
+
+                                <div className="form__group u-margin-bottom-medium">
+                                    <div className="form__radio-group">
+                                        <input type="radio" className="form__radio-input" id="small" name="size"/>
+                                        <label htmlFor="small" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Small tour group
+                                        </label>
+                                    </div>
+
+                                    <div className="form__radio-group">
+                                        <input type="radio" className="form__radio-input" id="large" name="size"/>
+                                        <label htmlFor="large" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Large tour group
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="form__group">
+                                    <button className="btn btn--green">Next Step &rarr;</button>
+                                </div>
+                                
                             </form>
                         </div>
                     </div>
