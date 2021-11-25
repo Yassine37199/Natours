d4f251c92c946a10509b545b5d2789ba05bf314e
Yassine37199 <yassine.ouri7319@gmail.com>
2021-11-25 05:35:04 +0100

    finished footer section

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import nat2 from './img/nat-2-large.jpg';
 import nat3 from './img/nat-3-large.jpg';
 import nat8 from './img/nat-8.jpg';
 import nat9 from './img/nat-9.jpg';
+import GreenLogo from './img/logo-green-2x.png';
 import videoMP4 from './img/video.mp4';
 import videoWebm from './img/video.webm';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -309,13 +310,46 @@ const MainPage = () => (
                                 <div className="form__group">
                                     <button className="btn btn--green">Next Step &rarr;</button>
                                 </div>
-                                
                             </form>
                         </div>
                     </div>
                 </div>
             </section>
         </main>
+
+        <footer className="footer">
+            <div className="footer__logo-box">
+                <img src={GreenLogo} alt="logo" className="footer__logo" />
+            </div>
+            <div className="row">
+                <div className="col-1-of-2">
+                    <div className="footer__navigation">
+                        <ul className="footer__list">
+                            <li className="footer__item">
+                                <a href="/" className="footer__link">Company</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="/" className="footer__link">Contact us</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="/" className="footer__link">Careers</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="/" className="footer__link">Privacy</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="/" className="footer__link">Terms</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-1-of-2">
+                    <p className="footer__copyright">
+                        Built By  <a href="/" className="footer__link">&copy; Yassine Ouri</a>  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem iure velit ducimus quos optio nobis error nihil cupiditate quisquam placeat. Quam fugit officiis dolorum nesciunt minima aspernatur, in ducimus vitae.
+                    </p>
+                </div>
+            </div>
+        </footer>
     </div>
 )
 
