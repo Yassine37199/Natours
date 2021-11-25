d8fa0c3b7a2ff4723f19bbc02a93a1bdfe061c6c
Yassine37199 <yassine.ouri7319@gmail.com>
2021-11-25 21:59:40 +0100

    started navigation button

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,23 @@ import { faGlobeEurope , faCompass, faMap , faHeart } from '@fortawesome/free-so
  
 const MainPage = () => (
     <div>
+        
+        <div className="navigation">
+            <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+            <label htmlFor="navi-toggle" className="navigation__button">Menu</label>
+
+            <div className="navigation__background">&nbsp;</div>
+            <nav className="navigation__nav">
+                <ul className="navigation__list">
+                    <li className="navigation__item"><a href="/" className="navigation__link">About Natours</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link">Your benefits</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link">Popular tours</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link">Stories</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link">Book now</a></li>
+                </ul>
+            </nav>
+        </div>
+
         <header className="header">
             <div className="logo-box">
                 <img src={logo} alt="Logo" className="logo" />
